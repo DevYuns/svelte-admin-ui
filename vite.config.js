37e1f8ddf305cf7e0cea8c9a4433a842b383ce24
DevYuns <ydhun93@gmail.com>
2022-01-10 15:31:43 +0900

    Test4

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import {svelte} from '@sveltejs/vite-plugin-svelte';
 const production = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
-  base: '/',
+  base: './',
   clearScreen: false,
   server: {port: 1337},
   plugins: [
